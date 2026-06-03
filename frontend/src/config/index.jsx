c85a603ce59e4b33efd17a0c32a112ee19ce7e77
Charan N Naik <charnnnaik24@gmail.com>
2026-06-03 17:14:58 +0530

    added url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-export const BASE_URL="https://careerlink-1-3bbo.onrender.com/";
+export const BASE_URL="https://careerlink-1-3bbo.onrender.com";
 export const clientserver=axios.create({
     baseURL:BASE_URL 
 })
